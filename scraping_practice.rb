65dc10cadfaf208e19f6eff15897ceb9eddee9c0
TSUYOSHI SEKIMOTO <166550623+user0131@users.noreply.github.com>
2024-07-10 12:24:49 +0000

    completed first scraping

--- scraping_practice.rb
+++ scraping_practice.rb
@@ -1,25 +1,49 @@
-require 'nokogiri'
-require 'open-uri'
+require 'net/http'
+require 'json'
+require 'uri'
 
-def fetch_article_info(url)
-  # URLからHTMLを取得
-  html = URI.open(url)
-  doc = Nokogiri::HTML(html)
+# ターゲットとなるURL
+url = 'https://www3.nhk.or.jp/news/json16/word/0001539_001.json?_=1720541485092'
 
-  # タイトルを取得
-  #title = doc.at_css('title').text
+# URLからJSONデータを取得
+uri = URI(url)
+response = Net::HTTP.get(uri)
+json_data = JSON.parse(response)
 
-  # 記事の本文を取得（例として、'grid--col--single'クラスを持つ要素をターゲットにします）
-  #article_body = doc.css('.grid--col--single').map(&:text).join("\n")
-  article_body = doc.css('#main > article.module.module--list-items.word-result > section > div > div > ul > li> a').each do |anchor|
-  puts anchor[:href]
+
+# 2番目のハッシュを取得
+first_hash = json_data[0] if json_data.is_a?(Array) && json_data.size > 0
+
+# ハッシュの中のハッシュの中の配列の中のハッシュから"title"キーの値を取得
+if first_hash
+  nested_array = first_hash['item'] 
+  if nested_hash && nested_hash.is_a?(Hash)
+    nested_array = nested_hash['nested_array_key'] # ここを実際のキー名に置き換えてください
+    if nested_array && nested_array.is_a?(Array) && nested_array.size > 0
+      title = nested_array[0]['title'] # ここを実際のキー名に置き換えてください
+      puts "Title: #{title}"
+    else
+      puts "配列が存在しないか、要素がありません。"
+    end
+  else
+    puts "ネストされたハッシュが存在しません。"
   end
-  # 結果を表示
-  #puts "タイトル: #{title}"
-  #puts "記事の本文:\n#{article_body}"
-  #puts article_body
+else
+  puts "2番目のハッシュが存在しません。"
 end
 
-# 実行例
-url = 'https://www3.nhk.or.jp/news/word/0001539.html'
-fetch_article_info(url)
+#puts json_data
+# 例: 'title'と'content'フィールドを抽出
+
+#title = json_data['title']
+#content = json_data['content']
+
+# 抽出した情報を表示
+#puts "Title: #{title}"
+#puts "Content: #{content}"
+
+# 必要に応じてJSON形式で保存
+#data_dict = { 'title' => title, 'content' => content }
+#File.open('extracted_data.json', 'w') do |f|
+#  f.write(JSON.pretty_generate(data_dict))
+#end
