c43d2f26d2c3990bc60582a7474b9e36a493981d
TSUYOSHI SEKIMOTO <166550623+user0131@users.noreply.github.com>
2024-07-12 05:53:03 +0000

    success in scraping title, time, url, and contents

---
+++ scraping_test3_success.rb
@@ -0,0 +1,38 @@
+require 'selenium-webdriver'
+
+# ChromeDriverのサービスを定義
+service = Selenium::WebDriver::Service.chrome(path: '/usr/local/bin/chromedriver')
+
+# Chromeのオプションを設定
+options = Selenium::WebDriver::Chrome::Options.new
+options.add_argument('--headless')  # ヘッドレスモードで実行
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+
+# WebDriverのインスタンスを作成
+driver = Selenium::WebDriver.for :chrome, options: options, service: service
+
+# ターゲットとなるURLにアクセス
+url = 'https://www3.nhk.or.jp/news/html/20240710/k10014507801000.html'
+driver.get(url)
+
+# 記事のタイトルを取得
+#title_element = driver.find_element(xpath: '//*[@id="main"]/article/header/h1')
+#title = title_element.text
+
+# 記事の本文を取得（最初の段落）
+content_element = driver.find_element(xpath: '//*[@id="main"]/article[2]/section/section/div/div/section/div/p[1]')
+content = content_element.text
+
+# 結果を表示
+#puts "Title: #{title}"
+puts "Content: #{content}"
+
+# 必要に応じて抽出したデータをJSON形式で保存
+#data_dict = { 'title' => title, 'content' => content }
+#File.open('scraped_article.json', 'w') do |f|
+#  f.write(JSON.pretty_generate(data_dict))
+#end
+
+# WebDriverを終了
+driver.quit
---
+++ scraping_test_success.rb
@@ -0,0 +1,44 @@
+require 'net/http'
+require 'json'
+require 'uri'
+
+# ターゲットとなるURL
+url = 'https://www3.nhk.or.jp/news/json16/word/0001539_001.json?_=1720541485092'
+
+# URLからJSONデータを取得
+uri = URI(url)
+response = Net::HTTP.get(uri)
+json_data = JSON.parse(response)
+
+# JSONデータの構造を確認（この行はデバッグ用です）
+# puts JSON.pretty_generate(json_data)
+# json_data = JSON.pretty_generate(json_data)
+# 1番目のハッシュの中の"item"キーの値である配列の中の1番目のハッシュの中の"title"キーの値を取得
+# ここではJSONデータの構造を仮定しています
+titles = []
+pubDates = []
+links = []
+first_hash = json_data.values.first # ハッシュの最初の値を取得
+items_array = first_hash['item'] if first_hash.is_a?(Hash) # "item"キーの値である配列を取得
+if items_array.is_a?(Array)
+  items_array.each do |item|
+    titles << item['title'] if item.is_a?(Hash) && item.key?('title')
+    pubDates << item['pubDate'] if item.is_a?(Hash) && item.key?('pubDate')
+    if item.is_a?(Hash) && item.key?('link')
+      detail_url = 'https://www3.nhk.or.jp' + item['link']
+      links << detail_url
+    end
+  end
+end
+news = {}
+
+# 取得した"title"の値を表示
+puts "Titles: #{titles}"
+puts "pubDates: #{pubDates}"
+puts "links: #{links}"
+
+# 必要に応じて抽出したタイトルをJSON形式で保存
+# data_dict = { 'title' => title }
+# File.open('extracted_title.json', 'w') do |f|
+#  f.write(JSON.pretty_generate(data_dict))
+# end
